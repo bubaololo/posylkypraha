8398dc4fb9ff5459fc2f5d89192eb9e653d6bbae
bubaololo <bubaololo@gmail.com>
2024-02-02 04:33:07 +0700

    form component

--- app/Livewire/OrderForm.php
+++ app/Livewire/OrderForm.php
@@ -2,10 +2,41 @@
 
 namespace App\Livewire;
 
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Livewire\Component;
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\MarkdownEditor;
 
-class OrderForm extends Component
+class OrderForm extends Component implements HasForms
 {
+    use InteractsWithForms;
+    
+    public ?array $data = [];
+    
+    public function mount(): void
+    {
+        $this->form->fill();
+    }
+    
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->required(),
+                MarkdownEditor::make('content'),
+                // ...
+            ])
+            ->statePath('data');
+    }
+    
+    public function create(): void
+    {
+        dd($this->form->getState());
+    }
+    
     public function render()
     {
         return view('livewire.order-form');
--- resources/views/livewire/order-form.blade.php
+++ resources/views/livewire/order-form.blade.php
@@ -1,3 +1,11 @@
 <div>
-    AZAZA
+    <form wire:submit="create">
+        {{ $this->form }}
+
+        <button type="submit">
+            Submit
+        </button>
+    </form>
+
+    <x-filament-actions::modals />
 </div>
