ebac3d4086ed61f21c7bdfa75d95b2846c158920
bubaololo <bubaololo@gmail.com>
2024-02-02 04:27:54 +0700

    draft

---
+++ resources/views/livewire/order-form.blade.php
@@ -0,0 +1,3 @@
+<div>
+    AZAZA
+</div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\CredentialController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\MockCartController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MainPageController;
 use App\Http\Controllers\ArticleController;
@@ -23,37 +24,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('products', [ProductController::class, 'productList'])->name('products.list');
-//Route::get('product/{slug}', [ProductController::class, 'singleProduct'])->name('single.product');
-//Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
-//Route::get('cart/{id}', [BuyNowController::class, 'buySpecificProduct'])->name('cart.now');
-//Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
-//Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
-//Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
-//Route::post('checkout', [CartController::class, 'store'])->name('cart.checkout');
-//Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
-//Route::post('delivery', [DeliveryCostController::class, 'getDeliveryCost'])->name('cdek');
+Route::get('products', [ProductController::class, 'productList'])->name('products.list');
+Route::get('product/{slug}', [ProductController::class, 'singleProduct'])->name('single.product');
+Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
+Route::get('cart/{id}', [BuyNowController::class, 'buySpecificProduct'])->name('cart.now');
+Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
+Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
+Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
+Route::post('checkout', [CartController::class, 'store'])->name('cart.checkout');
+Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
+Route::post('delivery', [DeliveryCostController::class, 'getDeliveryCost'])->name('cdek');
 
 
-//Route::get('profile',[ProfileController::class, 'index'])->name('profile');
+Route::get('profile',[ProfileController::class, 'index'])->name('profile');
 
-//Route::get('test', [MockCartController::class, 'store'])->name('cart.mock');
+Route::get('test', [MockCartController::class, 'store'])->name('cart.mock');
 
 //Route::get('/', [MainPageController::class, 'index'])->name('index'); // TODO найти где ещё есть обращение к имени роута и изменить его
+Route::get('/form', [OrderController::class, 'index'])->name('form'); // TODO найти где ещё есть обращение к имени роута и изменить его
 
 Route::get('/', function() {
-    return redirect('/blog');
+    return redirect('/form');
 })->name('index');
 
 Route::get('/switchLang/{lang}', [LanguageController::class, 'switchLang']);
 
-//Auth::routes();
+Auth::routes();
 
-//Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-//
-//Route::get('/order/{id}', [ProfileController::class, 'order'])->name('order.details');
-//
-//Route::resource('credentials', CredentialController::class);
-//Route::get('articles', [ArticleController::class, 'postsList'])->name('posts.list');
-//Route::get('{slug}', [ArticleController::class, 'show'])->name('post');
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+
+Route::get('/order/{id}', [ProfileController::class, 'order'])->name('order.details');
+
+Route::resource('credentials', CredentialController::class);
+Route::get('articles', [ArticleController::class, 'postsList'])->name('posts.list');
+Route::get('{slug}', [ArticleController::class, 'show'])->name('post');
 
